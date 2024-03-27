381f0d38d93b971e4bb38720b7e0e88b04e5b498
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2024-03-27 12:45:06 +0530

    Add files via upload

---
+++ frontend/src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <div
+        style={{
+          width: "100%",
+          minHeight: "20vh",
+          maxHeight: "30vh",
+          marginTop: 60,
+        }}
+      >
+        <p style={{ fontSize: "30px", textAlign: "center", padding: "20px" }}>
+          Built With love by
+          <span>
+           Deku
+          </span>
+          💘
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
